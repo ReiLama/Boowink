a7da6372bd5cbe7c36f7d5d8c714afacda6a70f5
elexbeats <skerdi.elezi@live.com>
2023-02-22 19:08:46 +0100

    boowink first commit

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# Boowink
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Boowink App Project</h1>
+    </div>
+  );
+}
+
+export default App;
