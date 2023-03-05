54d7a2ea13e439afa2a35aed57a71f5d4cd442c7
elexbeats <skerdi.elezi@live.com>
2023-03-05 10:02:06 +0100

    The main component that has the options to reserve everything.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
+import Tab from "./components/Tab/Tab";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Tab />
       <Footer />
     </div>
   );
