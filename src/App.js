dbf282c14f92464790e4df6a39acf724ec732a31
elexbeats <skerdi.elezi@live.com>
2023-04-06 20:59:01 +0200

    the latest commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer/Footer";
 import Card from "./components/Featured_content/Featured";
 import Tab from "./components/Tab/Tab";
 import Login from "./components/Login/Login";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
