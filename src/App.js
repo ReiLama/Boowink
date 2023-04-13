92978cee8a209579f446b4692cc5bf7297dcafd5
elexbeats <skerdi.elezi@live.com>
2023-04-13 21:35:10 +0200

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./components/Login/Signup";
 import Footer from "./components/Footer/Footer";
 
-import Footer from "./components/Footer/Footer";
-
 function App() {
   return (
     <div className="App">
@@ -18,13 +16,12 @@ function App() {
         <Routes>
           <Route path="/" element={<BodyContainer />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/Signup" element={<Signup/>}></Route>
-          <Route path="/Support" element={<Support/>}></Route>
+          <Route path="/Signup" element={<Signup />}></Route>
+          <Route path="/Support" element={<Support />}></Route>
           <Route path="/list" element={<List />}></Route>
         </Routes>
         <Footer />
       </Router>
-       
     </div>
   );
 }
