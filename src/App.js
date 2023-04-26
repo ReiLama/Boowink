0d6bde4e67f8f30dd0b5375bbf7a79444b80a5ff
elexbeats <skerdi.elezi@live.com>
2023-04-26 19:34:09 +0200

    added the barebones of user profile component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Signup from "./components/Login/Signup";
 import Footer from "./components/Footer/Footer";
 import Forgot from "./components/Login/Forgot";
 import ResPass from "./components/Login/ResPass";
+import UserProfile from "./components/UserProfile/UserProfile";
 
 function App() {
   return (
@@ -18,13 +19,14 @@ function App() {
         <Routes>
           <Route path="/" element={<BodyContainer />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/Signup" element={<Signup/>}></Route>
-          <Route path="/Forgot" element={<Forgot/>}></Route>
-          <Route path="/ResPass" element={<ResPass/>}></Route>
-          <Route path="/Support" element={<Support/>}></Route>
+          <Route path="/Signup" element={<Signup />}></Route>
+          <Route path="/Forgot" element={<Forgot />}></Route>
+          <Route path="/ResPass" element={<ResPass />}></Route>
+          <Route path="/Support" element={<Support />}></Route>
           <Route path="/hotel-details" element={<HotelDetails />}>
             <Route path="/hotel-details/:id" element={<HotelDetails />}></Route>
           </Route>
+          <Route path="/user-profile" element={<UserProfile />}></Route>
         </Routes>
         <Footer />
       </Router>
